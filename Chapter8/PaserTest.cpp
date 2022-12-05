d07b1fe5eac145d6dad0612250eb105c59486ef0
Local PC <lowlevelengineer@gmail.com>
2022-12-05 23:20:08 +0900

    origin

--- Chapter8/PaserTest.cpp
+++ Chapter8/PaserTest.cpp
@@ -17,14 +17,24 @@ int main(int argc, char *argv[])
 {
     char* message[];
 
-    if(argc < 1)
+    try
     {
-        std::cout << "ERR: 読み込むファイル名を指定してください" << std::endl;
-        return -1;
-    }
+        if(argc < 1)
+        {
+            std::cout << "ERR: 読み込むファイル名を指定してください" << std::endl;
+            return -1;
+        }
 
-    message = this.loadText(args[0]);
-    std::cout << "メッセージ読み込み成功" << std::endl;
+        message = this.loadText(args[0]);
+        std::cout << "メッセージ読み込み成功" << std::endl;
+    }
+    catch(IOException& ex)
+    {
+        std::cerr 
+            << "ERR: ファイル読み込み中にエラーが発生しました" 
+            << std::endl;
+        return;
+    }
 
     try
     {
@@ -36,7 +46,7 @@ int main(int argc, char *argv[])
             << pipMessage.getClass().getName();
             << std::endl;
     }
-    catch(const PraserException& e)
+    catch(const ParserException& ex)
     {
         std::cerr << e.what() << '\n';
     }
