469b7b73523b0610df01e91d961b025aba556d5c
Local PC <lowlevelengineer@gmail.com>
2022-12-10 22:07:06 +0900

    origin

--- PIP-Message/src/org/siprop/pip/message/util/Separators.cpp
+++ PIP-Message/src/org/siprop/pip/message/util/Separators.cpp
@@ -1,2 +1,28 @@
 
 
+class Separators final
+{
+public:
+    static const std::string SEMICOLON      = ";";
+    static const std::string COLON          = ":";
+    static const std::string COMMA          = ",";
+    static const std::string SLASH          = "/";
+    static const std::string SP             = " ";
+    static const std::string EQUALS         = "=";
+    static const std::string STAR           = "*";
+    static const std::string NEWLINE        = "\r\n";
+    static const std::string RETURN         = "\n";
+    static const std::string LESS_THAN      = "<";
+    static const std::string GREATER_THAN   = ">";
+    static const std::string AT             = "@";
+    static const std::string DOT            = ".";
+    static const std::string QUESTION       = "?";
+    static const std::string POUND          = "#";
+    static const std::string AND            = "&";
+    static const std::string LPAREN         = "(";
+    static const std::string RPAREN         = ")";
+    static const std::string DOUBLE_QUOTE   = "\"";
+    static const std::string QUOTE          = "\'";
+    static const std::string HT             = "\t";
+    static const std::string PERCENT        = "%";
+}
