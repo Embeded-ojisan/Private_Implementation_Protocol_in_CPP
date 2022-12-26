8d968c61c3cbb0e3d878db4d9579ac396f1751d3
Local PC <lowlevelengineer@gmail.com>
2022-12-26 22:31:35 +0900

    origin

--- Chapter8/readelf_all.sh
+++ Chapter8/readelf_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # https://www.server-memo.net/shellscript/read-file.html
+
+# 失敗作
+# 以下ではディレクトリごとに先頭の1ファイルのみ読み込み
 # find ../PIP-Message/build -name "*.o" | (read v ;readelf -a "$v" >> "$v.txt")
 
 find ../PIP-Message/build -name "*.o" | while read line
