c7ef038991a4c70db54293044eb010d6b10e2f61
Local PC <lowlevelengineer@gmail.com>
2022-12-27 23:25:01 +0900

    origin

--- PIP-Message/src/org/siprop/pip/message/include/parser/Lexer.hpp
+++ PIP-Message/src/org/siprop/pip/message/include/parser/Lexer.hpp
@@ -3,4 +3,21 @@
 
 #include "macro.hpp"
 
+#include "parser/Lexer.hpp"
+
+#include "util/LexerCore.hpp"
+
+#include <string>
+
+class Lexer: public LexerCore
+{
+public:
+    Lexer(std::string, std::string);
+
+    std::string getHeaderName(std::string);
+    std::string 
+
+private:
+};
+
 #endif // LEXER_H
